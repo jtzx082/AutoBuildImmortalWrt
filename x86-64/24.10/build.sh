23875a22753f7bb1e21562ba99ecadf93ec08c3a
jtzx082 <yujq804@gmail.com>
2025-01-26 10:55:15 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -48,6 +48,8 @@ PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn" 
 PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn" 
 PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+PACKAGES="$PACKAGES luci"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
